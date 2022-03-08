b30de366449cea009351151a5bf57b27577e24df
nassim <linnnux@hotmail.fr>
2022-03-08 22:36:54 +0100

    :v 0.6.3 : cors test

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb+srv://hello:world@cluster0.x3iff.mongodb.net/myFirstDa
 // cors config
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
