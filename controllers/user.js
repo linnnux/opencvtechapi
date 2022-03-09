a0f4e26736f58f07ff22dbbf4baaa94ddadf4bec
nassim <linnnux@hotmail.fr>
2022-03-09 08:34:09 +0100

    :v 0.6.7 : try bcryptjs

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,6 @@
 
 const User = require('../models/User');
-//const bcrypt = require('bcrypt');
-//const bcrypt = require('../app.js').bcrypt
-
-
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
